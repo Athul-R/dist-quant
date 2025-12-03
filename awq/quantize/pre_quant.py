fc1ba4298353a7dc3deeee0b6b98c8525f7924c4
Siddhant Mohan <siddhantmohan1110@gmail.com>
2025-12-03 11:13:25 -0500

    64 ppl

--- awq/quantize/pre_quant.py
+++ awq/quantize/pre_quant.py
@@ -250,5 +250,5 @@ def run_awq(
 
 
 def apply_awq(model, awq_results):
-    apply_scale(model, awq_results["scale"])
+    #apply_scale(model, awq_results["scale"])
     apply_clip(model, awq_results["clip"])
